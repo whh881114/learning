e8440eb5921e1f4b22cc600b17ede1778da064b1
Roy Wong <roy_wong_coding@163.com>
2024-10-10 16:39:45 +0800

    临时提交

--- go-projects/hello/hello.go
+++ go-projects/hello/hello.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
+	"greetings"
 	"log"
 )
 
 func main() {
-	// 设置预定义Logger的属性，包括jjfn
-	message := greetings.Hello("Gladys")
+	// 设置预定义Logger的属性，包括日志条目前缀和禁用打印的标志时间、源文件和行号。
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+	// 如果返回错误，则将期打印到控制台并退出程序。
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 如果没有返回错误，则打印返回的消息到控制台。
 	fmt.Println(message)
 }
